bdd8179561711a18d0e3382a3b958b499c086dbe
sethvincent <sethvincent@gmail.com>
2014-04-25 13:14:48 -0700

    forgot the before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_https
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
